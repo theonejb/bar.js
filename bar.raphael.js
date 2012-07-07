ec2f927c02c045e2a86ba548c39a3473a5eadcef
Asad Jibran Ahmed <surfer.a1@gmail.com>
2012-07-07 21:30:21 +0500

    single series plotting working
    features:
    - basic plotting
    - text labels
    - auto spacing for single series data
    
    next:
    - update(data) function with animation capabilities

--- bar.raphael.js
+++ bar.raphael.js
@@ -11,6 +11,7 @@
         var graph_obj = {
             paper: this,
             meta_set: this.set(), // holds meta graph objects, like axis lines...
+            label_set: this.set(),
             bar_set: this.set(),
 
             width: this.width,
@@ -18,30 +19,58 @@
         }; 
 
         var def_opts = {
-            left_gutter: 20,
-            right_gutter: 20,
-            bottom_gutter: 20,
-            top_gutter: 20,
-            axis_width: 2,
+            graph: {
+                top_gutter: 20,
+                right_gutter: 20,
+                bottom_gutter: 20,
+                left_gutter: 20,
+            },
 
-            bar_width: 20,
-            bar_spacing: 5,
-            group_spacing: 10,
+            axis: {
+                width: 2
+            },
+
+            labels: {
+                font: "Terminal Grotesque",
+                font_size: 12
+            },
+
+            auto_size: true, // auto calculate bar size and spacing b/w elements
+
+            bar: {
+                width: 20
+            },
+
+            spacing:{
+                bar: 5,
+                group: 10
+            },
+
+            yscale: {
+                top_gutter: 50 // added to max value in data to get highest axis tick
+            }
         };
         graph_obj.opts = $.extend({}, def_opts, opts);
 
-        var init_graph = function () {
+        var init_graph = function (grouped) {
+            if (r.is(grouped, "undefined")) {
+                gouped = false;
+            }
+
             this.paper.clear();
+            this.meta_set.clear();
+            this.label_set.clear();
+            this.bar_set.clear();
 
             // draw axis lines on the paper
             var axis_data = {
-                x_left: this.opts.left_gutter,
-                x_right: this.paper.width - this.opts.right_gutter,
-                x_bottom: this.paper.height - this.opts.bottom_gutter,
+                x_left: this.opts.graph.left_gutter,
+                x_right: this.paper.width - this.opts.graph.right_gutter,
+                x_bottom: this.paper.height - this.opts.graph.bottom_gutter,
 
-                y_left: this.opts.left_gutter,
-                y_top: this.opts.top_gutter,
-                y_bottom: this.paper.height - this.opts.bottom_gutter,
+                y_left: this.opts.graph.left_gutter,
+                y_top: this.opts.graph.top_gutter,
+                y_bottom: this.paper.height - this.opts.graph.bottom_gutter,
             }
 
             // move to (x_left, x_bottom) and draw a HLine to (x_right, x_bottom)
@@ -52,15 +81,15 @@
             // draw lines and set their width
             var x_line = this.paper.path(x_path_string),
                 y_line = this.paper.path(y_path_string);
-            x_line.attr("stroke-width", this.opts.axis_width);
-            y_line.attr("stroke-width", this.opts.axis_width);
+            x_line.attr("stroke-width", this.opts.axis.width);
+            y_line.attr("stroke-width", this.opts.axis.width);
             this.meta_set.push(x_line, y_line)
 
             this.draw_surface = this.ds = {
-                left: this.opts.left_gutter + this.opts.axis_width + this.opts.group_spacing,
-                top: this.opts.top_gutter,
-                right: this.width - this.opts.right_gutter,
-                bottom: this.height - (this.opts.bottom_gutter + this.opts.axis_width)
+                left: this.opts.graph.left_gutter + this.opts.axis.width,
+                top: this.opts.graph.top_gutter,
+                right: this.width - this.opts.graph.right_gutter,
+                bottom: this.height - (this.opts.graph.bottom_gutter + this.opts.axis.width)
             };
 
             return this;
@@ -73,55 +102,121 @@
          * lables: array of strings. labels.length == data.length
          */
         var draw = function (data, labels) {
-            this.bar_set.clear();
-
             var grouped = r.is(data[0], "array");
+            var yscale = $.extend(this.opts.yscale, this.get_yscale(data, grouped));
+            if (!grouped) {
+                var draw_labels = (labels.length == data.length),
+                    label_pos = [];
+            }
+
+            this.init(grouped);
+
+            if (this.opts.auto_size) {
+                this.opts.spacing = this.auto_space(data.length, grouped);
+            }
+
+            var el_width = 0;
             if (grouped) {
-                var group_width = (this.opts.bar_width * data.length) +
-                    (this.opts.bar_spacing * (data.length - 1)) + 
-                    this.opts.group_spacing;
+                // TODO: calculate group width here
             }
             else {
-                var total_bar_width = this.opts.bar_width + this.opts.bar_spacing;
+                el_width = this.opts.spacing.bar + this.opts.bar.width;
             }
 
-            var draw_labels = (labels.length == data.length);
-            var yscale = this._get_yscale(data, grouped);
 
             if (!grouped) {
                 for (var i = 0; i < data.length; i++) {
-                    var bar_height = data[i] * yscale;
+                    var bar_height = data[i] * yscale.scale;
                     var bar_y = this.ds.bottom - bar_height;
-                    var bar_x = this.ds.left + (total_bar_width * i + 1) + 
-                        this.opts.bar_spacing;
-                    var bar = this.paper.rect(bar_x, bar_y, this.opts.bar_width,
+                    var bar_x = this.ds.left + (el_width * i) + 
+                        this.opts.spacing.bar;
+                    var bar = this.paper.rect(bar_x, bar_y, this.opts.bar.width,
                                               bar_height);
                     this.bar_set.push(bar);
+
+                    if (draw_labels) {
+                        var bar_bbox = bar.getBBox();
+                        var bar_midx = bar_bbox.x + 
+                            Math.round((bar_bbox.x2-bar_bbox.x)/2);
+                        label_pos.push(bar_midx);
+                    }
                 }
+            }
+
+            if (draw_labels) {
+                this.draw_labels(labels, label_pos);
             }
         };
         graph_obj.draw = $.proxy(draw, graph_obj);
 
+        var draw_labels = function (labels, pos) {
+            var font = this.paper.getFont(this.opts.labels.font);
+            for (var i = 0; i < labels.length; i++) {
+                var x = pos[i],
+                    y = (this.height - this.opts.graph.bottom_gutter +
+                         this.opts.axis.width) + Math.floor(this.opts.labels.font_size/2);
+                var label = this.paper.text(x, y, labels[i]).attr(
+                    "font-size", this.opts.labels.font_size
+                );
+                /*
+                var label = this.paper.print(x, y, labels[i], font);
+
+                // calculate new center position and move label to there
+                var bb = label.getBBox();
+                var w = bb.x2 - bb.x;
+                var x_diff =  -1 * (w/2);
+                label.transform("t" + x_diff + ",0");
+                */
+
+                this.label_set.push(label);
+            }
+        };
+        graph_obj.draw_labels = $.proxy(draw_labels, graph_obj);
+
+        /*
+         * get_yscale ([] data, bool grouped)
+         *
+         * @param grouped: if grouped, the min/max calc. is done differently
+         *
+         * @returns {scale, min, max}
+         */
         var get_yscale = function (data, grouped) {
-            var max = 0, min = 0;
+            var min, max;
             if (!grouped) {
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i] > max) {
-                        max = data[i];
-                    }
-                    if (data[i] < min) {
-                        min = data[i];
-                    }
-                }
+                min = Math.min.apply(null, data);
+                max = Math.max.apply(null, data);
             }
 
-            var diff = max - min,
+            var diff = (max+this.opts.yscale.top_gutter) - min,
                 y_height = this.ds.bottom - this.ds.top,
                 y_scale = y_height / diff;
 
-            return y_scale;
+            return {
+                scale: y_scale,
+                min: min,
+                max: (max+this.opts.yscale.top_gutter)
+            };
         };
-        graph_obj._get_yscale = $.proxy(get_yscale, graph_obj);
+        graph_obj.get_yscale = $.proxy(get_yscale, graph_obj);
+
+        /*
+         * auto_sapce (int data_len, bool grouped)
+         *
+         * @returns null
+         */
+        var auto_space = function (data_len, grouped) {
+            var ds_width = this.ds.right - this.ds.left;
+            var spacing = {};
+
+            if (!grouped) {
+                var total_bars_width = data_len * this.opts.bar.width,
+                    free_space = ds_width - total_bars_width,
+                    bar_spacing = Math.floor(free_space/(data_len+1));
+
+                return {bar: bar_spacing};
+            }
+        };
+        graph_obj.auto_space = $.proxy(auto_space, graph_obj);
 
         return graph_obj.init();
     };
